b4ae98ffdf98e8897a7ba807efa8e0adabcec5dd
Kevin Jin <goldstar.kevin@gmail.com>
2015-04-05 19:20:20 -0400

    Case 3 implementation

--- indexCase/src/IndexCaseSample.java
+++ indexCase/src/IndexCaseSample.java
@@ -1,4 +1,8 @@
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.uchicago.index.core.AbstractIndexCase;
@@ -15,42 +19,60 @@ import com.optionscity.freeway.api.IJobSetup;
  * @author Kevin Jin
  */
 public class IndexCaseSample extends AbstractIndexCase implements IndexCase {
+	private static final DecimalFormat FMT = new DecimalFormat("0.00");
+
+	private static final int[][] ROUND_1_CORRELATION_ORDER = {
+		{26,	15,	28,	4,	2,	5,	21,	11,	1,	8,	19,	16,	14,	20,	29,	23,	9,	22,	0,	6,	17,	18,	7,	10,	12,	25,	3,	27,	24,	13,	},
+		{15,	13,	11,	25,	0,	18,	21,	20,	7,	27,	8,	23,	10,	9,	26,	1,	3,	29,	14,	4,	2,	19,	16,	22,	12,	24,	6,	17,	28,	5,	},
+		{9,	4,	17,	13,	0,	21,	16,	26,	12,	18,	6,	19,	25,	22,	7,	24,	2,	5,	15,	23,	1,	3,	20,	10,	27,	8,	14,	11,	29,	28,	},
+		{6,	25,	8,	9,	24,	18,	3,	21,	12,	17,	1,	4,	29,	14,	5,	2,	10,	15,	20,	26,	28,	23,	27,	7,	22,	16,	0,	19,	11,	13,	},
+		{20,	2,	19,	8,	0,	15,	12,	13,	28,	21,	5,	22,	18,	4,	24,	16,	3,	7,	29,	6,	1,	25,	26,	14,	10,	27,	17,	11,	23,	9,	},
+		{21,	26,	23,	0,	19,	29,	15,	18,	14,	16,	4,	8,	6,	7,	12,	11,	5,	22,	2,	24,	27,	25,	3,	9,	28,	13,	17,	20,	1,	10,	},
+		{19,	7,	10,	26,	11,	3,	17,	2,	29,	5,	6,	24,	20,	15,	0,	14,	18,	4,	23,	28,	16,	25,	27,	1,	22,	21,	12,	8,	9,	13,	},
+		{9,	25,	11,	6,	26,	21,	18,	1,	19,	2,	29,	5,	23,	7,	17,	0,	10,	4,	13,	27,	16,	22,	28,	8,	3,	15,	12,	14,	24,	20,	},
+		{11,	19,	12,	4,	27,	3,	0,	10,	1,	18,	5,	14,	25,	9,	8,	16,	23,	22,	15,	26,	17,	7,	24,	13,	2,	29,	6,	20,	21,	28,	},
+		{7,	25,	2,	29,	24,	21,	15,	3,	18,	23,	11,	16,	0,	1,	8,	9,	22,	17,	13,	20,	28,	14,	5,	27,	19,	26,	12,	6,	10,	4,	},
+		{6,	13,	24,	17,	26,	22,	23,	8,	15,	1,	11,	29,	10,	25,	7,	19,	20,	0,	27,	14,	3,	2,	28,	4,	18,	16,	21,	9,	5,	12,	},
+		{8,	7,	18,	6,	19,	23,	12,	1,	26,	0,	17,	24,	9,	14,	10,	5,	11,	20,	27,	29,	28,	25,	21,	16,	13,	3,	2,	22,	4,	15,	},
+		{21,	13,	14,	28,	24,	23,	11,	8,	25,	4,	17,	2,	18,	27,	16,	5,	12,	22,	3,	29,	20,	19,	0,	1,	7,	6,	9,	15,	26,	10,	},
+		{15,	18,	12,	23,	1,	25,	10,	20,	29,	2,	24,	17,	21,	4,	22,	27,	13,	26,	7,	9,	14,	16,	5,	8,	28,	11,	19,	3,	0,	6,	},
+		{12,	19,	20,	5,	0,	8,	11,	26,	18,	28,	14,	24,	21,	23,	6,	3,	1,	9,	13,	27,	4,	10,	16,	22,	29,	7,	17,	2,	25,	15,	},
+		{13,	29,	1,	16,	0,	28,	4,	23,	9,	25,	27,	26,	5,	10,	24,	22,	20,	15,	2,	6,	21,	8,	17,	18,	3,	19,	7,	12,	11,	14,	},
+		{15,	26,	22,	2,	23,	25,	0,	28,	5,	27,	17,	9,	19,	18,	12,	16,	4,	8,	24,	7,	29,	21,	1,	13,	14,	6,	10,	3,	11,	20,	},
+		{27,	18,	2,	13,	26,	29,	10,	6,	12,	11,	19,	25,	16,	24,	17,	3,	21,	0,	7,	9,	15,	8,	22,	23,	20,	5,	14,	1,	28,	4,	},
+		{13,	25,	11,	17,	23,	22,	7,	1,	5,	19,	2,	9,	12,	24,	8,	3,	4,	16,	21,	14,	18,	0,	6,	28,	29,	26,	15,	20,	10,	27,	},
+		{25,	6,	11,	21,	8,	14,	4,	28,	5,	0,	18,	17,	22,	7,	2,	16,	19,	24,	26,	10,	29,	20,	12,	1,	15,	9,	3,	27,	13,	23,	},
+		{21,	4,	13,	29,	23,	28,	14,	27,	0,	1,	24,	15,	20,	11,	6,	10,	12,	19,	9,	25,	2,	3,	18,	17,	22,	8,	5,	26,	7,	16,	},
+		{12,	5,	20,	19,	29,	22,	13,	9,	2,	7,	23,	0,	25,	1,	4,	18,	21,	3,	14,	17,	15,	24,	16,	27,	26,	28,	6,	10,	11,	8,	},
+		{29,	28,	21,	16,	10,	18,	25,	19,	13,	4,	2,	0,	15,	22,	5,	12,	9,	8,	7,	1,	17,	24,	26,	6,	3,	23,	14,	20,	11,	27,	},
+		{13,	27,	12,	11,	24,	15,	29,	5,	21,	18,	16,	20,	10,	26,	0,	9,	1,	7,	23,	14,	2,	8,	6,	25,	3,	17,	22,	28,	4,	19,	},
+		{12,	23,	13,	9,	10,	11,	3,	20,	18,	15,	17,	27,	2,	24,	19,	6,	5,	14,	4,	29,	16,	26,	21,	25,	22,	28,	8,	1,	7,	0,	},
+		{7,	9,	18,	19,	13,	12,	3,	15,	21,	28,	1,	29,	16,	22,	17,	2,	8,	25,	27,	10,	5,	4,	23,	26,	20,	24,	6,	0,	11,	14,	},
+		{0,	6,	7,	5,	29,	17,	16,	2,	10,	11,	27,	15,	23,	28,	14,	1,	26,	19,	13,	18,	24,	8,	25,	4,	3,	22,	21,	9,	20,	12,	},
+		{23,	17,	8,	15,	26,	20,	1,	16,	13,	24,	12,	27,	25,	28,	7,	5,	10,	11,	14,	21,	9,	3,	6,	2,	29,	19,	4,	0,	18,	22,	},
+		{12,	29,	0,	15,	22,	19,	25,	20,	4,	16,	26,	14,	28,	27,	18,	6,	9,	5,	7,	3,	10,	24,	11,	21,	13,	17,	23,	2,	1,	8,	},
+		{15,	22,	28,	13,	21,	9,	20,	26,	23,	17,	5,	25,	0,	6,	7,	10,	29,	24,	12,	1,	4,	18,	19,	3,	16,	11,	27,	8,	14,	2,	},
+	};
 	/* Personal latest copy of information */
-	private double[] my_portfolioWeights;
+	private double[] my_portfolioWeights = new double[30];
 	Map<Integer, boolean[]> timeMap = new ConcurrentHashMap<Integer, boolean[]>();
+	Set<Integer> forbidden = new HashSet<Integer>();
+	int[][] correlationOrder = ROUND_1_CORRELATION_ORDER;
 
-	private double[] evenlyDistributeWeightsAcrossTradables(boolean[] tradables) {
-		// Help function that evenly divide weight across every tradable asset
-
-		// 1. Find how many assets we can hold
-		int numberTradable = 0;
-		for (int i = 0; i < 30; i++) {
-			if (tradables[i]) {
-				numberTradable++;
-			}
-		}
-
-		// 2. Calculate even weight
-		double evenWeight = 1.0 / numberTradable;
-
-		// 3. Allocate that weight to the assets we're allowed to trade
-		double[] portfolioWeights = new double[30];
-		for (int i = 0; i < 30; i++) {
-			if (tradables[i]) {
-				portfolioWeights[i] = evenWeight;
-			}
-		}
-
-		// 4. Return portfolio weights
-		return portfolioWeights;
-	}
+	private double[] weights;
+	private double[] lastPrices;
 
 	@Override
 	public double[] initalizePosition(double[] underlyingPrices, double indexValue, double[] trueWeights, boolean[] tradables) {
 		// We just distribute portfolio weights evenly across every tradable asset
-		my_portfolioWeights = evenlyDistributeWeightsAcrossTradables(tradables);
+		forbidden.clear();
+		for (int i = 0; i < 30; i++)
+			if (!tradables[i])
+				forbidden.add(Integer.valueOf(i));
 
-		return my_portfolioWeights;
+		weights = trueWeights;
+		lastPrices = Arrays.copyOf(underlyingPrices, 30);
+		return Arrays.copyOf(trueWeights, 30);
 	}
 
 	@Override
@@ -59,8 +81,35 @@ public class IndexCaseSample extends AbstractIndexCase implements IndexCase {
 		if (timeMap.containsKey(currentTime)) {
 			// Time is up...need to redistribute or I'll get penalties!!!
 			boolean[] newTradables = timeMap.get(currentTime);
-			my_portfolioWeights = evenlyDistributeWeightsAcrossTradables(newTradables);
+			forbidden.clear();
+			for (int i = 0; i < 30; i++)
+				if (!newTradables[i])
+					forbidden.add(Integer.valueOf(i));
 		}
+		double commission = 0.031399675;
+		double[] add = new double[30];
+		double synthetic = 0;
+		double actual = 0;
+		String str = "";
+		for (int j = 0; j < 30; j++) {
+			if (forbidden.contains(Integer.valueOf(j))) {
+				int sub;
+				for (sub = 0; forbidden.contains(Integer.valueOf(correlationOrder[j][sub])); sub++)
+					sub = correlationOrder[j][sub];
+				double ratio;
+				ratio = (lastPrices[j] * weights[j]) / (lastPrices[correlationOrder[j][sub]] * weights[correlationOrder[j][sub]]);
+				add[correlationOrder[j][sub]] = ratio;
+				my_portfolioWeights[j] = 0;
+			} else {
+				my_portfolioWeights[j] = (1 + add[j]);
+			}
+			synthetic += my_portfolioWeights[j] * underlyingPrices[j] * weights[j];
+			str += (FMT.format(my_portfolioWeights[j] * underlyingPrices[j] * weights[j]) + " ");
+			actual += underlyingPrices[j] * weights[j];
+		}
+		str += FMT.format(synthetic * (1 - commission)) + " " + FMT.format(actual * (1 - commission)) + " " + FMT.format((synthetic / actual - 1) * 100) + "%";
+		log(currentTime + " " + str);
+		lastPrices = Arrays.copyOf(underlyingPrices, 30);
 		return my_portfolioWeights;
 	}
 
--- indexCase/src/Test.java
+++ indexCase/src/Test.java
@@ -1,9 +1,208 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NavigableMap;
 import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Test {
+	public static class LinearRegression {
+		private final int N;
+		private final double alpha, beta;
+		private final double R2;
+		private final double svar, svar0, svar1;
+		private final double correlation;
+
+		/**
+		 * Performs a linear regression on the data points <tt>(y[i], x[i])</tt>.
+		 * 
+		 * @param x
+		 *            the values of the predictor variable
+		 * @param y
+		 *            the corresponding values of the response variable
+		 * @throws java.lang.IllegalArgumentException
+		 *             if the lengths of the two arrays are not equal
+		 */
+		public LinearRegression(List<Double> x, List<Double> y) {
+			if (x.size() != y.size()) {
+				throw new IllegalArgumentException("array lengths are not equal");
+			}
+			N = x.size();
+
+			// first pass
+			double sumx = 0.0, sumy = 0.0, sumx2 = 0.0;
+			for (int i = 0; i < N; i++)
+				sumx += x.get(i).doubleValue();
+			for (int i = 0; i < N; i++)
+				sumx2 += x.get(i).doubleValue() * x.get(i).doubleValue();
+			for (int i = 0; i < N; i++)
+				sumy += y.get(i).doubleValue();
+			double xbar = sumx / N;
+			double ybar = sumy / N;
+
+			// second pass: compute summary statistics
+			double xxbar = 0.0, yybar = 0.0, xybar = 0.0;
+			for (int i = 0; i < N; i++) {
+				xxbar += (x.get(i).doubleValue() - xbar) * (x.get(i).doubleValue() - xbar);
+				yybar += (y.get(i).doubleValue() - ybar) * (y.get(i).doubleValue() - ybar);
+				xybar += (x.get(i).doubleValue() - xbar) * (y.get(i).doubleValue() - ybar);
+			}
+			beta = xybar / xxbar;
+			alpha = ybar - beta * xbar;
+			correlation = (xybar / (N - 1)) / (Math.sqrt(yybar / (N - 1)) * Math.sqrt(xxbar / (N - 1)));
+
+			// more statistical analysis
+			double rss = 0.0; // residual sum of squares
+			double ssr = 0.0; // regression sum of squares
+			for (int i = 0; i < N; i++) {
+				double fit = beta * x.get(i).doubleValue() + alpha;
+				rss += (fit - y.get(i).doubleValue()) * (fit - y.get(i).doubleValue());
+				ssr += (fit - ybar) * (fit - ybar);
+			}
+
+			int degreesOfFreedom = N - 2;
+			R2 = ssr / yybar;
+			svar = rss / degreesOfFreedom;
+			svar1 = svar / xxbar;
+			//svar0 = svar / N + xbar * xbar * svar1;
+			svar0 = svar * sumx2 / (N * xxbar);
+		}
+
+		/**
+		 * Returns the <em>y</em>-intercept &alpha; of the best of the best-fit line <em>y</em> = &alpha; + &beta; <em>x</em>.
+		 * 
+		 * @return the <em>y</em>-intercept &alpha; of the best-fit line <em>y = &alpha; + &beta; x</em>
+		 */
+		public double intercept() {
+			return alpha;
+		}
+
+		/**
+		 * Returns the slope &beta; of the best of the best-fit line <em>y</em> = &alpha; + &beta; <em>x</em>.
+		 * 
+		 * @return the slope &beta; of the best-fit line <em>y</em> = &alpha; + &beta; <em>x</em>
+		 */
+		public double slope() {
+			return beta;
+		}
+
+		public double correlation() {
+			return correlation;
+		}
+
+		/**
+		 * Returns the coefficient of determination <em>R</em><sup>2</sup>.
+		 * 
+		 * @return the coefficient of determination <em>R</em><sup>2</sup>, which is a real number between 0 and 1
+		 */
+		public double R2() {
+			return R2;
+		}
+
+		/**
+		 * Returns the standard error of the estimate for the intercept.
+		 * 
+		 * @return the standard error of the estimate for the intercept
+		 */
+		public double interceptStdErr() {
+			return Math.sqrt(svar0);
+		}
+
+		/**
+		 * Returns the standard error of the estimate for the slope.
+		 * 
+		 * @return the standard error of the estimate for the slope
+		 */
+		public double slopeStdErr() {
+			return Math.sqrt(svar1);
+		}
+
+		/**
+		 * Returns the expected response <tt>y</tt> given the value of the predictor variable <tt>x</tt>.
+		 * 
+		 * @param x
+		 *            the value of the predictor variable
+		 * @return the expected response <tt>y</tt> given the value of the predictor variable <tt>x</tt>
+		 */
+		public double predict(double x) {
+			return beta * x + alpha;
+		}
+
+		/**
+		 * Returns a string representation of the simple linear regression model.
+		 * 
+		 * @return a string representation of the simple linear regression model, including the best-fit line and the coefficient of determination <em>R</em><sup>2</sup>
+		 */
+		public String toString() {
+			String s = "";
+			s += String.format("%.2f N + %.2f", slope(), intercept());
+			return s + "  (R^2 = " + String.format("%.3f", R2()) + ")";
+		}
+	}
+
+	private static double[] getWeights() throws FileNotFoundException {
+		double[] weights = new double[30];
+		try (Scanner scan = new Scanner(new File("market-data/round1/capWeights.csv"))) {
+			for (int i = 0; i < 30; i++)
+				weights[i] = scan.nextDouble();
+		}
+		return weights;
+	}
+
+	//these are hardcoded based on historical data
+	private static int[][] findHighestCorrelating(int round) throws FileNotFoundException {
+		List<List<Double>> percentChanges = new ArrayList<>();
+		for (int i = 0; i < 30; i++)
+			percentChanges.add(new ArrayList<Double>());
+		try (Scanner scan = new Scanner(new File("market-data/round" + round + "/prices.csv"))) {
+			scan.nextLine(); //skip headers line
+
+			double[] prev = new double[30];
+			String[] line0 = scan.nextLine().split(",");
+			for (int j = 0; j < 30; j++)
+				prev[j] = Double.parseDouble(line0[j]);
+			for (int i = 0; i < 9999; i++) {
+				String[] line1 = scan.nextLine().split(",");
+				for (int j = 0; j < 30; j++) {
+					double now = Double.parseDouble(line1[j]);
+					percentChanges.get(j).add(Double.valueOf((now - prev[j]) / prev[j]));
+					prev[j] = now;
+				}
+			}
+		}
+
+		final double[][] correlations = new double[30][30];
+		for (int i = 0; i < 29; i++)
+			for (int j = i + 1; j < 30; j++)
+				correlations[i][j] = correlations[j][i] = new LinearRegression(percentChanges.get(i), percentChanges.get(j)).correlation();
+
+		int[][] orders = new int[30][30];
+		for (int i = 0; i < 30; i++) {
+			final int iAt = i;
+			SortedSet<Integer> sorter = new TreeSet<Integer>(new Comparator<Integer>() {
+				@Override
+				public int compare(Integer j1, Integer j2) {
+					if (correlations[iAt][j1.intValue()] < correlations[iAt][j2.intValue()])
+						return 1;
+					else if (correlations[iAt][j1.intValue()] > correlations[iAt][j2.intValue()])
+						return -1;
+					else
+						return 0;
+				}
+			});
+			for (int j = 0; j < 30; j++)
+				sorter.add(Integer.valueOf(j));
+			int j = 0;
+			for (Integer counterPart : sorter)
+				orders[i][j++] = counterPart.intValue();
+		}
+		return orders;
+	}
+
 	public static void main(String[] args) throws FileNotFoundException {
 		IndexCaseSample c = new IndexCaseSample() {
 			@Override
@@ -18,25 +217,60 @@ public class Test {
 				System.out.println(s);
 			}
 		};
-		DecimalFormat FMT = new DecimalFormat("0.00");
+		int[][] correlationOrder = findHighestCorrelating(1);
+		System.out.println("BEGIN CORRELATION ORDER");
+		for (int i = 0; i < 30; i++) {
+			System.out.print(i + 1 + ": {");
+			for (int j = 0; j < 30; j++)
+				System.out.print(correlationOrder[i][j] + 1 + ",\t");
+			System.out.println("}");
+		}
+		System.out.println("END CORRELATION ORDER");
+
+		double[] weights = getWeights();
+		double[][] oldNominals = new double[1000][30];
 		try (Scanner scan = new Scanner(new File("market-data/round1/prices.csv"))) {
 			scan.nextLine(); //skip headers line
 
-			double[] prev = new double[30];
-			String[] line0 = scan.nextLine().split(",");
-			for (int j = 0; j < 30; j++)
-				prev[j] = Double.parseDouble(line0[j]);
-			System.out.println("BEGIN PERCENT CHANGES IN PRICE");
-			for (int i = 0; i < 999; i++) {
-				String[] line1 = scan.nextLine().split(",");
+			for (int i = 0; i < oldNominals.length; i++) {
+				String[] line = scan.nextLine().split(",");
 				for (int j = 0; j < 30; j++) {
-					double now = Double.parseDouble(line1[j]);
-					System.out.print(FMT.format((now - prev[j]) / prev[j] * 1000) + "\t");
-					prev[j] = now;
+					double price = Double.parseDouble(line[j]);
+					oldNominals[i][j] = price;
 				}
-				System.out.println();
 			}
-			System.out.println("END PERCENT CHANGES IN PRICE");
+		}
+		double commission = 0.031399675;
+		boolean[] tradables = new boolean[30];
+		try (Scanner scan = new Scanner(new File("market-data/round1/tradable_init.csv"))) {
+			for (int i = 0; i < 30; i++) {
+				int line = scan.nextInt();
+				tradables[i] = line == 1;
+			}
+		}
+		NavigableMap<Integer, int[]> tradableChanges = new TreeMap<>();
+		try (Scanner scan = new Scanner(new File("market-data/round1/tradable_changes.csv"))) {
+			while (scan.hasNext()) {
+				String[] line = scan.nextLine().split(",");
+				tradableChanges.put(Integer.valueOf(line[0]) - 1, new int[] { Integer.parseInt(line[1]), Integer.parseInt(line[2]) });
+			}
+		}
+		double index = 0;
+		for (int j = 0; j < 30; j++)
+			index += oldNominals[0][j];
+		index *= (1 - commission);
+		c.initalizePosition(oldNominals[0], index, weights, tradables);
+		for (int i = 1; i < oldNominals.length; i++) {
+			int[] change = tradableChanges.get(i);
+			if (change != null) {
+				tradables[change[0]] = change[1] == 1;
+				c.regulationAnnouncement(i, i + 20, tradables);
+			}
+			index = 0;
+			for (int j = 0; j < 30; j++)
+				index += oldNominals[i][j];
+			index *= (1 - commission);
+			c.updatePosition(i, oldNominals[i], index);
 		}
 	}
 }
